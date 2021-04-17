f6e7fd5fd16400acaf8939bdf585dd682445e9b7
Nivethiha/Demo <nivethiha.kannan@lntinfotech.com>
2021-04-17 16:09:56 +0530

    Emi card

--- src/app/component/shopping-main/emi-card/emi-card.component.ts
+++ src/app/component/shopping-main/emi-card/emi-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import{FormControl,FormGroup}from '@angular/forms';
 
 @Component({
   selector: 'app-emi-card',
@@ -6,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./emi-card.component.css']
 })
 export class EmiCardComponent implements OnInit {
+  emicardForm=new FormGroup({
+    cardnumber: new FormControl(''),
+    limit:new FormControl(''),
+    balance:new FormControl(''),
+    cardtype: new FormControl(''),
+    validtill: new FormControl(''),
+
+    
+  })
 
   constructor() { }
 
+  get cardnumber() {
+    return this.emicardForm.get('cardnumber');
+  } 
+  get limit() {
+    return this.emicardForm.get('limit');
+  } 
+  get validtill() {
+    return this.emicardForm.get('validtill');
+  } 
+  get balance() {
+    return this.emicardForm.get('balance');
+  } 
+  get cardtype() {
+    return this.emicardForm.get('cardtype');
+  } 
   ngOnInit(): void {
   }
 
